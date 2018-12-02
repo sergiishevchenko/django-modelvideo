f34fd65e323a586b8380f11477e9d899915c9ab8
sergii <sergiishevchenko017@gmail.com>
2018-12-02 14:05:53 +0300

    first commit

--- example/models.py
+++ example/models.py
@@ -6,3 +6,11 @@ class Company(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Programmer(models.Model):
+    name = models.CharField(max_length=20)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
