a0f247a8b275555147c616c5046e97086c1a4de3
sergii <sergiishevchenko017@gmail.com>
2018-12-02 13:17:27 +0300

    first commit

--- django_modelvideo/settings.py
+++ django_modelvideo/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'example'
+    'example',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
