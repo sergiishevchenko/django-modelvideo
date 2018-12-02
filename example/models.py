c1a24078096530fae82587dd1210065d85bc596d
sergii <sergiishevchenko017@gmail.com>
2018-12-02 14:40:59 +0300

    first commit

--- example/models.py
+++ example/models.py
@@ -8,9 +8,20 @@ class Company(models.Model):
         return self.name
 
 
-class Programmer(models.Model):
+class Language(models.Model):
     name = models.CharField(max_length=20)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+
+class Programmer(models.Model):
+    name = models.CharField(max_length=20)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    language = models.ManyToManyField(Language)
+
+    def __str__(self):
+        return self.name
+
+
+
