93c807f6693bccc6b1f676154bc68ff405920f65
sergii <sergiishevchenko017@gmail.com>
2018-12-02 19:02:14 +0300

    first commit

--- query/models.py
+++ query/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+    date_created = models.DateField() 
 
     def __str__(self):
         return self.name
@@ -10,6 +12,9 @@ class Company(models.Model):
 
 class Language(models.Model):
     name = models.CharField(max_length=20)
+    creator = models.CharField(max_length=20)
+    paradigm = models.CharField(max_length=20)
+    date_created = models.DateField()
 
     def __str__(self):
         return self.name
@@ -17,11 +22,9 @@ class Language(models.Model):
 
 class Programmer(models.Model):
     name = models.CharField(max_length=20)
+    age = models.IntegerField()
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    language = models.ManyToManyField(Language)
+    languages = models.ManyToManyField(Language)
 
     def __str__(self):
         return self.name
-
-
-
